b1900497c4af43298e83f032ee934157d5955a5e
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-27 11:28:23 +0400

    Is written pydoc for tokenize wrapper-function and token expressions list.

--- src/Lexer/Language/lexer.py
+++ src/Lexer/Language/lexer.py
@@ -1,12 +1,24 @@
 from src.Lexer.lexer import lexer
 
+"""
+RESERVED is language expressions.
+INT is whole numbers (integers).
+ID is identifiers (e. g. variable names).
+"""
 RESERVED = 'RESERVED'
 INT = 'INT'
 ID = 'ID'
 
+"""
+'None' - whitespace characters. It's ignored when parsing.
+'RESERVED' - language expressions.
+'INT' - whole numbers (integers).
+'ID' - identifiers.
+"""
 token_expressions = [
     (r'[ \n\t]+', None),
     (r'#[^\n]*', None),
+
     (r'\:=', RESERVED),
     (r'\(', RESERVED),
     (r'\)', RESERVED),
@@ -30,10 +42,14 @@ token_expressions = [
     (r'while', RESERVED),
     (r'do', RESERVED),
     (r'end', RESERVED),
+
     (r'[0-9]+', INT),
+
     (r'[A-Za-z][A-Za-z0-9_]*', ID)
 ]
 
-
-def tokenize(characters):
-    return lexer(characters, token_expressions)
+"""
+Function-wrapper to run the Lexer (with the token expressions listed here).
+"""
+def tokenize(code):
+    return lexer(code, token_expressions)
