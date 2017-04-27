c635e4bce5b4c22787dd040ce44d8c6e2d94fc77
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-27 12:04:59 +0400

    Is written base class for combinators.

--- src/Parser/combinators.py
+++ src/Parser/combinators.py
@@ -7,3 +7,11 @@ class Result:
     def __init__(self, value, position):
         self.value = value
         self.position = position
+
+"""
+Base class for combinators.
+__call__ will override by subclasses.
+"""
+class Combinator:
+    def __call__(self, tokens, position):
+        return None
