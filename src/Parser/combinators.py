b28b2e11ed74a2584e17848aa2d394d3dd62bdc3
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-30 10:38:23 +0300

    Is written Process combinator.

--- src/Parser/combinators.py
+++ src/Parser/combinators.py
@@ -119,3 +119,17 @@ class Rep(Combinator):
             position = result.position
             result = self.parser(tokens, position)
         return Result(results, position)
+
+"""
+'Process' applies a function to the result of the parser, if it is successful.
+"""
+class Process(Combinator):
+    def __init__(self, parser, function):
+        self.parser = parser
+        self.function = function
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result:
+            result.value = self.function(result.value)
+            return result
