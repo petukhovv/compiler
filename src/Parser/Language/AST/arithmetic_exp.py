8f5d25bc1b27dcd07b43d862c23408b626614e66
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-30 11:53:14 +0300

    Is written variable & binary operation arithmetic expression class for AST.

--- src/Parser/Language/AST/arithmetic_exp.py
+++ src/Parser/Language/AST/arithmetic_exp.py
@@ -6,6 +6,7 @@ class Aexp(Equality):
 """
 Integer arithmetic expression class for AST.
 eval - runtime function for Evaluator (just return i).
+Example: 54
 """
 class IntAexp(Aexp):
     def __init__(self, i):
@@ -17,3 +18,26 @@ class IntAexp(Aexp):
     def eval(self, env):
         return self.i
 
+"""
+Variable arithmetic expression class for AST.
+Example: x
+"""
+class VarAexp(Aexp):
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'VarAexp(%s)' % self.name
+
+"""
+Binary operation arithmetic expression class for AST.
+Example: x + 54
+"""
+class BinopAexp(Aexp):
+    def __init__(self, op, left, right):
+        self.op = op
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'BinopAexp(%s, %s, %s)' % (self.op, self.left, self.right)
