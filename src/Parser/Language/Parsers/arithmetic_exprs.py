fa0a090b3681a1ee0c83d101dda1125ca3ef2778
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-30 15:15:17 +0300

    Is written precedence levels for arithmetic operations.

--- src/Parser/Language/Parsers/arithmetic_exprs.py
+++ src/Parser/Language/Parsers/arithmetic_exprs.py
@@ -3,6 +3,22 @@ from src.Parser.Language.AST.arithmetic_exprs import *
 from src.Parser.Language.Parsers.basic import *
 
 """
+
+"""
+def any_operator_in_list(ops):
+    op_parsers = [keyword(op) for op in ops]
+    parser = reduce(lambda l, r: l | r, op_parsers)
+    return parser
+
+"""
+Precedence levels for arithmetic operations.
+"""
+aexp_precedence_levels = [
+    ['*', '/'],
+    ['+', '-'],
+]
+
+"""
 Converts the values returned by 'num' and 'id' to the object of AST classes.
 First of all, try to parse integer, if unsuccessful, try to parse as a variable (via Alternate combinator).
 """
