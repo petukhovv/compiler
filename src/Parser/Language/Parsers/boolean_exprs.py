a50c99bbb7888b48c6bfb9cf669d34ad829fc68a
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-30 15:56:34 +0300

    Is written binary expression group parser and main binary expression parser.

--- src/Parser/Language/Parsers/boolean_exprs.py
+++ src/Parser/Language/Parsers/boolean_exprs.py
@@ -1,5 +1,5 @@
 from src.Parser.Language.AST.boolean_exprs import *
-from src.Parser.Language.Parsers.arithmetic_exprs import aexp, any_operator_in_list
+from src.Parser.Language.Parsers.arithmetic_exprs import aexp, any_operator_in_list, process_group
 
 from src.Parser.Language.Parsers.basic import *
 
@@ -22,3 +22,18 @@ Parsing 'not' expression (convert expression to object of AST-class 'NotBexp').
 """
 def bexp_not():
     return keyword('not') + Lazy(bexp_term) ^ (lambda parsed: NotBexp(parsed[1]))
+
+"""
+Parse the binary expression in parentheses.
+"""
+def bexp_group():
+    return keyword('(') + Lazy(bexp) + keyword(')') ^ process_group
+
+"""
+Parse the binary expression.
+Try to first parse as 'not' expression,
+if not possible - as just binary expressions,
+if not possible - as a parentheses group of binary expressions.
+"""
+def bexp_term():
+    return bexp_not() | bexp_relop() | bexp_group()
