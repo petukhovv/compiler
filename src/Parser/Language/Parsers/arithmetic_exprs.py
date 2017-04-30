e7327bb3f16428b4c8972b1ac3d3361a109f9d7c
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-30 13:25:55 +0300

    Is written aexp_group parser.

--- src/Parser/Language/Parsers/arithmetic_exprs.py
+++ src/Parser/Language/Parsers/arithmetic_exprs.py
@@ -4,7 +4,21 @@ from src.Parser.Language.Parsers.basic import *
 
 """
 Converts the values returned by 'num' and 'id' to the object of AST classes.
+First of all, try to parse integer, if unsuccessful, try to parse as a variable (via Alternate combinator).
 """
 def aexp_value():
     return (num ^ (lambda i: IntAexp(i))) | \
            (id ^ (lambda v: VarAexp(v)))
+
+"""
+Removes parentheses and returns an expression in them.
+"""
+def process_group(parsed):
+    ((_, p), _) = parsed
+    return p
+
+"""
+Parse the arithmetic expression in parentheses.
+"""
+def aexp_group():
+    return keyword('(') + Lazy(aexp) + keyword(')') ^ process_group
