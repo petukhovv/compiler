3ce4eb9abdceb5024d79d0c477a70b91abad12f3
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-30 16:09:28 +0300

    Is written statement list parser.

--- src/Parser/Language/Parsers/statements.py
+++ src/Parser/Language/Parsers/statements.py
@@ -12,3 +12,14 @@ def assign_stmt():
         ((name, _), exp) = parsed
         return AssignStatement(name, exp)
     return id + keyword(':=') + aexp() ^ process
+
+"""
+Parsing statement list (by ';' separator).
+Example:
+x := 56;
+y := 12;
+z := 512
+"""
+def stmt_list():
+    separator = keyword(';') ^ (lambda x: lambda l, r: CompoundStatement(l, r))
+    return Exp(stmt(), separator)
