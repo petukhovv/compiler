9871b90253f04c3204b03b29bb88eaa50f62e7d3
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-30 23:09:46 +0300

    Is written pydoc for parse and parser functions.

--- src/Parser/Parsers/general.py
+++ src/Parser/Parsers/general.py
@@ -1,10 +1,15 @@
 from src.Parser.Parsers.statements import stmt_list
 from src.Parser.combinators import *
 
-
+"""
+Run the parser with the input tokens from the 0-position.
+"""
 def parse(tokens):
     ast = parser()(tokens, 0)
     return ast
 
+"""
+Run the top-level parser (statement list).
+"""
 def parser():
     return Phrase(stmt_list())
