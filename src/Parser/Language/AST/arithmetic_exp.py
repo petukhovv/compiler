7dcbd8ab8d6d106079eea7bd575af8792fc8090f
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-30 11:45:51 +0300

    Is written integer arithmetic expression class for AST.

---
+++ src/Parser/Language/AST/arithmetic_exp.py
@@ -0,0 +1,14 @@
+from equality import *
+
+class Aexp(Equality):
+    pass
+
+class IntAexp(Aexp):
+    def __init__(self, i):
+        self.i = i
+
+    def __repr__(self):
+        return 'IntAexp(%d)' % self.i
+
+    def eval(self, env):
+        return self.i
