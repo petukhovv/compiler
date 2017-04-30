daeb40d6019a558af203390fbe6b6f5138e0e18b
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-30 12:05:25 +0300

    Is written OrBexp and NotBexp.

--- src/Parser/Language/AST/boolean_exprs.py
+++ src/Parser/Language/AST/boolean_exprs.py
@@ -30,3 +30,30 @@ class AndBexp(Bexp):
 
     def __repr__(self):
         return 'AndBexp(%s, %s)' % (self.left, self.right)
+
+"""
+'Or' operation boolean expression class for AST.
+Example: x < 11 or x > 100
+"""
+class OrBexp(Bexp):
+        def __init__(self, left, right):
+            self.left = left
+            self.right = right
+
+        def __repr__(self):
+            return 'OrBexp(%s, %s)' % (self.left, self.right)
+
+"""
+'Not' operation boolean expression class for AST.
+Example: x not 11
+"""
+class NotBexp(Bexp):
+    def __init__(self, exp):
+        self.exp = exp
+
+    def __repr__(self):
+        return 'NotBexp(%s)' % self.exp
+
+    def eval(self, env):
+        value = self.exp.eval(env)
+        return not value
