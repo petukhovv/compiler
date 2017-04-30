82c3c448d2b15f49530536666d5aceba662fe873
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-04-30 15:50:22 +0300

    Is written 'not' expression parser.

--- src/Parser/Language/Parsers/boolean_exprs.py
+++ src/Parser/Language/Parsers/boolean_exprs.py
@@ -1,6 +1,8 @@
 from src.Parser.Language.AST.boolean_exprs import *
 from src.Parser.Language.Parsers.arithmetic_exprs import aexp, any_operator_in_list
 
+from src.Parser.Language.Parsers.basic import *
+
 """
 Convert boolean expression value to object of AST-class 'RelopBexp'.
 """
@@ -14,3 +16,9 @@ Parsing boolean expression (arithmetic expression + compare operator + arithmeti
 def bexp_relop():
     relops = ['<', '<=', '>', '>=', '=', '!=']
     return aexp() + any_operator_in_list(relops) + aexp() ^ process_relop
+
+"""
+Parsing 'not' expression (convert expression to object of AST-class 'NotBexp').
+"""
+def bexp_not():
+    return keyword('not') + Lazy(bexp_term) ^ (lambda parsed: NotBexp(parsed[1]))
