1657e0544f3d2453b1c080198b074cb592865353
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-08-26 12:30:26 +0300

    memory_cycle commands generator implementation

--- src/Compiler/VM/Helpers/assembler.py
+++ src/Compiler/VM/Helpers/assembler.py
@@ -17,11 +17,11 @@ class Commands(list):
         return commands_relation_map[command] + argument
 
     """
-    Генерация команд для организация цикла.
+    Генерация команд для организация цикла на стеке.
     Цикл завершается, если на стеке оказалось 0.
     После завершения на стек помещается число совершенных итераций.
     """
-    def cycle(self, env, callback, load_counter=True):
+    def stack_cycle(self, env, callback, load_counter=True):
         # Создаем метки и переменные, необходимые для прохождения цикла.
         start_while_label = Environment.create_label(env)
         end_while_label = Environment.create_label(env)
@@ -42,7 +42,7 @@ class Commands(list):
         self.add(Load, counter_var)\
             .add(Push, 1)\
             .add(Add)\
-            .add(Store, counter_var)\
+            .add(Store, counter_var)
 
         # Если после выполнения callback на стеке 0 - завершаем цикл.
         self.add(Dup)\
@@ -52,6 +52,51 @@ class Commands(list):
 
         # Очищаем оставшийся на стеке 0 (поскольку перед Jz использовали Dup).
         self.add(Pop)
+
+        # Если требуется, загружаем на стек количество совершенных итераций.
+        if load_counter:
+            self.add(Load, counter_var)
+
+        return self
+
+    """
+    Генерация команд для организация цикла в памяти.
+    Цикл завершается, если в очередной ячейке памяти оказалось 0.
+    После завершения на стек помещается число совершенных итераций.
+    """
+    def memory_cycle(self, env, var_number, callback=None, load_counter=True):
+        # Создаем метки и переменные, необходимые для прохождения цикла.
+        start_while_label = Environment.create_label(env)
+        end_while_label = Environment.create_label(env)
+        continue_label = Environment.create_label(env)
+        counter_var = Environment.create_var(env)
+
+        # Инициализируем счетчик цикла.
+        self.add(Push, 0)\
+            .add(Store, counter_var)\
+            .add(Label, start_while_label)
+
+        # Выполняем тело цикла.
+        if callback is not None:
+            callback(counter_var, end_while_label, continue_label)
+
+        self.add(Label, continue_label)
+
+        # Если после выполнения callback в ячейке памяти 0 - завершаем цикл.
+        self.add(Load, var_number)\
+            .add(Load, counter_var)\
+            .add(Sub) \
+            .add(BLoad, 0)\
+            .add(Jz, end_while_label)
+
+        # Инкрементируем счетчик цикла.
+        self.add(Load, counter_var) \
+            .add(Push, 1) \
+            .add(Add) \
+            .add(Store, counter_var)
+
+        self.add(Jump, start_while_label)\
+            .add(Label, end_while_label)
 
         # Если требуется, загружаем на стек количество совершенных итераций.
         if load_counter:
