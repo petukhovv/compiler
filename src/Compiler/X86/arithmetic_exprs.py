e4a6f98d171081b6f60aa98b7bb625b86efc080e
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-09-30 14:33:56 +0300

    Added another arithmetic operations

--- src/Compiler/X86/arithmetic_exprs.py
+++ src/Compiler/X86/arithmetic_exprs.py
@@ -2,6 +2,26 @@
 
 from pprint import pprint
 
+""" Мапа: арифметический оператор в языке программирования - арифметический оператор в ASM-коде """
+binop_compare_map = {
+    '+': {
+        'operator': 'add',
+        'operands': ['eax', 'ebx']
+    },
+    '-': {
+        'operator': 'sub',
+        'operands': ['eax', 'ebx']
+    },
+    '*': {
+        'operator': 'mul',
+        'operands': ['ebx']
+    },
+    '/': {
+        'operator': 'div',
+        'operands': ['ebx']
+    }
+}
+
 """ Компиляция числа """
 def int_aexp(compiler, i):
     compiler.code.add('mov', [compiler.target_register, i])
@@ -13,4 +33,4 @@ def binop_aexp(compiler, op, left, right):
     compiler.target_register = 'ebx'
     right.compile_x86(compiler)
 
-    compiler.code.add('add', ['eax', 'ebx'])
+    compiler.code.add(binop_compare_map[op]['operator'], binop_compare_map[op]['operands'])
