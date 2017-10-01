348495d1d254a19482d11c6d39ac6ab901aaf7ab
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-10-01 21:06:58 +0300

    Added negative values support in itoa

--- src/Compiler/X86/Utils/asm/atoi.asm
+++ src/Compiler/X86/Utils/asm/atoi.asm
@@ -7,23 +7,23 @@ _atoi:
         jz	._atoi_exit
         cmp dl, 10          ; 10 - ASCII code of EOL (\n)
         je ._atoi_exit
-        cmp dl, 45          ; 10 - ASCII code of -
-        je ._atoi_invert_mark
+        cmp dl, 45          ; 45 - ASCII code of - (minus - mark for negative value)
+        je ._atoi_negative_mark
         imul	eax, 10
         sub	dl, 48          ; 48 - ASCII code of 0
         add	eax, edx
         inc	esi
         jmp	._atoi_start
-._atoi_invert_mark:
-    mov ebx, 1
-    inc	esi
-    jmp	._atoi_start
-._atoi_invert:
-    neg eax
-    mov ebx, 0
-    jmp ._atoi_exit
-._atoi_exit:
-    cmp ebx, 1
-    je ._atoi_invert
-    mov dl, 0
-	ret
+    ._atoi_negative_mark:
+        mov ebx, 1
+        inc	esi
+        jmp	._atoi_start
+    ._atoi_negative:
+        neg eax
+        mov ebx, 0
+        jmp ._atoi_exit
+    ._atoi_exit:
+        cmp ebx, 1
+        je ._atoi_negative
+        mov dl, 0
+        ret
